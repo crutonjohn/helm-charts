7d1caa1c6391d4cf1aed17557da97b38f650532c
Curtis John <admin@curtisrayjohn.com>
2021-01-09 22:17:18 -0500

    fix helm-docs location

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-HELM_DOCS_VERSION="0.11.0"
+HELM_DOCS_VERSION="1.4.0"
 
 # install helm-docs
-curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/instrumenta/norwoodj/helm-docs/download/"${HELM_DOCS_VERSION}"/helm-docs-linux-amd64.tar.gz
+curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v${HELM_DOCS_VERSION}/helm-docs_1.4.0_Linux_x86_64.tar.gz
 tar -xf /tmp/helm-docs.tar.gz helm-docs
 
 # validate docs
